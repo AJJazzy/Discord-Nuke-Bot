7cc0d36ce6aaee192c9af6a1b4363d21bd1aae5a
AJJazzy <148536926+AJJazzy@users.noreply.github.com>
2023-10-20 05:54:56 -0800

    Data.py emits customizability to the nuke bot.
    This file is mainly just the customizability of the last file, this file sets the "NukeMessage" variable (The message being sent onto the channels)
    It also sets the "ChannelName" variable (The Name of the channels being made!)
    Its also about how many channels being deleted
    And it also sets the amount of channels to be made!

---
+++ data.py
@@ -0,0 +1,9 @@
+channelName = "YourChannelName"  # The name of the channels being made.
+DeleteAllChannels = 100  # Please set this to one option, what this does is it decides whether you want to delete all channels or not. (100 is max amount of channels.) (Set this to a lower number if you want to delete only a certain amount of channels.)
+NukeMessage = "Your Nuke Message"  # The message being sent to the channels
+AmountOfchannels = 10  # Please Set this to the amount of channels you want to create, the bigger the amount of channels is the more pings they get by the second.
+#Recommended Settings Have Been Inputted automatically, if you wish to change them please be cautious of the discord global rate limit.
+#This is a togglable nuke bot meaning that you can do .nuke again in any of the channels to stop this from happening, and please do not use this for anything that may harm anyone or a server
+#Please beware that raiding a server is against the discord TOS and this is ONLY for educational purposes.
+                                                                    #For this to work properly please make sure that the bot token you have is valid, and if you are still having issues, feel free to join the
+                                                                    #Discord VIA this invite link! https://discord.gg/ukXcKTTMP7
